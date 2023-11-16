2f0e908670a6890f34acc47a5a244dfa5da04d76
sdas1 <sdas1@miraclesoft.com>
2023-11-16 17:58:12 +0530

    backend connection

--- connection.js
+++ connection.js
@@ -1,5 +1,18 @@
 const mysql=require('mysql');
 require('dotenv').config();
 var connection=mysql.createConnection({
-    
+    port:process.env.DB_PORT,
+    host:process.env.DB_HOST,
+    user:process.env.DB_USERNAME,
+    password:process.env.DB.password,
+    database:process.env.DB_NAME
+
+})
+connection.connect((err)=>{
+    if (!err) {
+        console.log('connectedf');
+        
+    } else {
+        console.log(err);
+    }
 })
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express =require('express');
 var cors=require('cors');
+const connection=require('./connection');
 const app =express();
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
